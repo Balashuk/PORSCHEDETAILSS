14cf3155b9b35b0d2e14b1f68ac3340f8e8b2c17
Yaroslav Balashuk <jaoslav.balashuk@gmail.com>
2025-01-25 20:13:27 +0200

    first

--- app/api/product/[id]/route.ts
+++ app/api/product/[id]/route.ts
@@ -1,17 +1,26 @@
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import { NextResponse } from "next/server";
+import prisma from "@/libs/prismadb"; // Не забудьте подключить prisma
 
-export async function DELETE(request:Request,{params}:{params:{id:string}}) {
-    const currentUser=await getCurrentUser()
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+    const currentUser = await getCurrentUser();
 
-    if(!currentUser) return NextResponse.error()
+    if (!currentUser) {
+        return NextResponse.error(); // Возвращаем ошибку, если пользователь не авторизован
+    }
 
-        if( currentUser.role!=="ADMIN"){
-            return NextResponse.error()
-        }
+    if (currentUser.role !== "ADMIN") {
+        return NextResponse.error(); // Возвращаем ошибку, если пользователь не имеет роли ADMIN
+    }
 
-    const product =await prisma?.product.delete({
-        where:{id:params.id}
-    })
-    return NextResponse.json(product)
+    try {
+        const product = await prisma.product.delete({
+            where: { id: params.id },
+        });
+
+        return NextResponse.json(product); // Возвращаем данные удаленного продукта
+    } catch (error) {
+        console.error("Error deleting product:", error);
+        return NextResponse.error(); // Возвращаем ошибку, если не удалось удалить продукт
+    }
 }
